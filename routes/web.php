90387378b37866e0b224d7e2318931845ced1638
kynoprueba <kynfestivo@gmail.com>
2021-02-23 10:53:31 -0500

    iniciamos repositorio sonocapsulas

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 
-//Route::get('contactos', function () {
-   // return "regreso de contacto";
-//})->name('inicio');
-
-Route::view('/', 'home')->name('home');
